ac3e5ace8ed6417241d23d7634c852fadaec2ad4
siddharth <144979580+Sid141004@users.noreply.github.com>
2024-09-09 10:30:40 +0530

    Create up_down.c

---
+++ lab-7/up_down.c
@@ -0,0 +1,31 @@
+#include<lpc17xx.h>
+int i;
+int j;
+int x;
+//cnb-1->switch p2.12
+int main(){
+	SystemInit();
+	SystemCoreClockUpdate();
+	LPC_PINCON->PINSEL0=0;
+	LPC_GPIO0->FIODIR=0XFF<<4;
+	
+	while(1){
+		x = LPC_GPIO2->FIOPIN;
+		x = x & 1<<12;
+		if(x==0){
+			LPC_GPIO0->FIOPIN=i<<4;
+			i+=1;
+			if(i==255){
+				i=0;
+			}
+		}
+		else{
+			LPC_GPIO0->FIOPIN=i<<4;
+			i-=1;
+			if(i==0){
+				i=255;
+			}
+		}
+		for(j=0;j<100000;j++);
+	}
+}
